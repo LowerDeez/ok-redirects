18c073c7a1b6115980b2359f793a16b6f0745859
Oleg <igorkleschunov@gmail.com>
2020-09-04 12:14:58 +0300

    Fix typo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ pkj_name = 'redirects'
 
 setup(
     name='django-ok-redirects',
-    version='0.1.4',
+    version='0.2',
     long_description_content_type='text/x-rst',
     packages=[pkj_name] + [pkj_name + '.' + x for x in find_packages(pkj_name)],
     include_package_data=True,
