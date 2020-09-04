9e0a4697e5d0a2fbcf1061d08cfe4a014b6bdea1
Oleg <igorkleschunov@gmail.com>
2020-09-04 14:37:13 +0300

    fix name in config

--- ok_redirects/apps.py
+++ ok_redirects/apps.py
@@ -3,5 +3,5 @@ from django.utils.translation import pgettext_lazy
 
 
 class RedirectsConfig(AppConfig):
-    name = 'redirects'
+    name = 'ok_redirects'
     verbose_name = pgettext_lazy("ok:redirects", "Redirects")
