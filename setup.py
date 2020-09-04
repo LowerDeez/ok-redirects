a529331bd5fc64b7b7c988b3c3cb36214e85b89b
Oleg <igorkleschunov@gmail.com>
2020-09-04 14:37:33 +0300

    Bump version 0.3.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ pkj_name = 'ok_redirects'
 
 setup(
     name='django-ok-redirects',
-    version='0.3',
+    version='0.3.1',
     long_description_content_type='text/x-rst',
     packages=[pkj_name] + [pkj_name + '.' + x for x in find_packages(pkj_name)],
     include_package_data=True,
