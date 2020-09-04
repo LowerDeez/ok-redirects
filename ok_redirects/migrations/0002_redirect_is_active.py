781c7cd00e9221dcbfa21a6997ed35b178532b93
Oleg <igorkleschunov@gmail.com>
2020-09-04 14:47:49 +0300

    fix migration

--- ok_redirects/migrations/0002_redirect_is_active.py
+++ ok_redirects/migrations/0002_redirect_is_active.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('redirects', '0001_initial'),
+        ('ok_redirects', '0001_initial'),
     ]
 
     operations = [
